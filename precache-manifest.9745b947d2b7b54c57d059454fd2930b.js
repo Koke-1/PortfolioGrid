67804651dbc2c974e72512d83ea34b6bf106867e
Koke-1 <sanjuangeorge@gmail.com>
2021-03-13 20:17:10 -0600

    Updates

---
+++ precache-manifest.9745b947d2b7b54c57d059454fd2930b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4b74d8d9814b8389599c2f0d9421dd",
+    "url": "/PortfolioGrid/index.html"
+  },
+  {
+    "revision": "6de9686ee17ae96019fa",
+    "url": "/PortfolioGrid/static/css/main.30c5a460.chunk.css"
+  },
+  {
+    "revision": "7abfe7ede776268f3da3",
+    "url": "/PortfolioGrid/static/js/2.6b08c3bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortfolioGrid/static/js/2.6b08c3bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de9686ee17ae96019fa",
+    "url": "/PortfolioGrid/static/js/main.75cf014b.chunk.js"
+  },
+  {
+    "revision": "db1a8fe22c454515acd4",
+    "url": "/PortfolioGrid/static/js/runtime-main.9320f41f.js"
+  }
+]);
